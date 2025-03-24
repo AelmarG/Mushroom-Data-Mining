0863a4d5ebad5fd0e65c66c59dea4942b7ef9a3e
Aelmar Gewarges <75628741+AelmarG@users.noreply.github.com>
2025-03-24 11:00:14 -0400

    Update mushroom-data.py

--- mushroom-data.py
+++ mushroom-data.py
@@ -1,12 +1,14 @@
-
-# Imports
+#Imports
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 #import the data and label the columns as they are unlabelled
 mushrooms = pd.read_csv('./agaricus-lepiota.data',
-                        names=['cap-shape','cap-surface','cap-color',
+                        names=['classification','cap-shape','cap-surface','cap-color',
                                'bruises?','odor','gill-attachment',
                                'gill-spacing','gill-size','gill-color',
                                'stalk-shape','stalk-root','stalk-surface-above-ring',
@@ -20,7 +22,26 @@ mushrooms = pd.read_csv('./agaricus-lepiota.data',
 #turn any '?' into a NaN
 mushrooms = mushrooms.replace(to_replace='?', value=np.nan)
 
-#drop every row that is missing data
+#drop every row that is missing data (rows with NaN)
 mushrooms = mushrooms.dropna()
 
-print(mushrooms)
+
+###CLASSIFICATION METHOD 1: DECISION TREES
+
+#split the attributes of the data frame into the class value and all other attributes
+X = mushrooms.drop(['classification'], axis=1)
+Y = mushrooms['classification']
+
+#split X and Y into testing and training sets
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, 
+    test_size=0.2,        #testing is 20% of the data
+    stratify=Y,           #preserve class distribution
+    random_state=42       #ensures same split every run
+)
+
+###CLASSIFICATION METHOD 2: NAIVE BAYES
+
+
+
+####CLASSIFICATOIN METHOD 3: SUPPORT VECTOR MACHINES
